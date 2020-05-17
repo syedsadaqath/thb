f6e46404383bc1560568b48f6ee677ee23bc065b
syed sadakhath khalander <sadhakhath47@gmail.com>
2020-05-17 08:36:45 +0530

    commit changes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -20,5 +20,45 @@ namespace THB_site.Controllers
         {
             return View();
         }
+        public ActionResult Orientation_Center()
+        {
+            return View();
+        }
+        public ActionResult Innovation_Center()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult Careers()
+        {
+            return View();
+        }
+        public ActionResult Industries()
+        {
+            return View();
+        }
+        public ActionResult Our_clients()
+        {
+            return View();
+        }
+        public ActionResult Our_Tech_Expertise()
+        {
+            return View();
+        }
+        public ActionResult Technical_Services()
+        {
+            return View();
+        }
+        public ActionResult Staffing()
+        {
+            return View();
+        }
+        public ActionResult Contact_Us()
+        {
+            return View();
+        }
     }
 }
